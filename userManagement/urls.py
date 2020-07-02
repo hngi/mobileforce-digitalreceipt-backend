eeac6fb77a9453ecb80763616168c3c56c05bc3d
Eniwoke Cornelius <eniwokepdm@gmail.com>
2020-07-02 12:30:18 +0100

    feat: add view to get all notifications for User

--- userManagement/urls.py
+++ userManagement/urls.py
@@ -1,16 +1,27 @@
 from django.urls import path
 
-from .views import user_registration_send_email, create_user, login, change_password, check_if_user_exists, \
-    create_notification, send_notification_now, logout, forgot_password
+from .views import (
+    user_registration_send_email,
+    create_user,
+    login,
+    change_password,
+    check_if_user_exists,
+    create_notification,
+    send_notification_now,
+    logout,
+    forgot_password,
+    get_all_notifications,
+)
 
 urlpatterns = [
-    path('otp_register', user_registration_send_email),
-    path('register', create_user),
-    path('login', login),
-    path('change_password', change_password),
-    path('forgot_password', forgot_password),
-    path('email/exists', check_if_user_exists),
-    path('logout', logout),
-    path('notification/create', create_notification),
-    path('notification/create/send/now', send_notification_now),
+    path("otp_register", user_registration_send_email),
+    path("register", create_user),
+    path("login", login),
+    path("change_password", change_password),
+    path("forgot_password", forgot_password),
+    path("email/exists", check_if_user_exists),
+    path("logout", logout),
+    path("notification/create", create_notification),
+    path("notification/all", get_all_notifications),
+    path("notification/create/send/now", send_notification_now),
 ]
