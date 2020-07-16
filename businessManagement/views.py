6c0d59008340677039573db0e1ea61b799ab0794
Samuel Bamgbose <bsaintdesigns@gmail.com>
2020-07-16 08:39:41 +0100

    update tax and discount

--- businessManagement/views.py
+++ businessManagement/views.py
@@ -74,6 +74,8 @@ def add_product_info_to_receipt(request):
             "name": request.data["name"],
             "quantity": request.data["quantity"],
             "unit_price": request.data["unit_price"],
+            "tax_amount": request.data["tax_amount"],
+            "discount": request.data["discount"]
         }
         serializer = ProductSerializer(data=data)
         if serializer.is_valid():
