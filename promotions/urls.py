267160ff340255d25bd3a8c9a5ecfaa9d7662a5f
Adah Eni <eniwokepdm@gmail.com>
2020-07-20 09:50:15 +0100

    refactor: rename url_patterns to urlpatterns

--- promotions/urls.py
+++ promotions/urls.py
@@ -1,4 +1,8 @@
-from .views import add_promotion
 from django.urls import path
+from .views import create_promotion, update_promotion, delete_promotion
 
-url_patterns = [path("promotion/create", add_promotion)]
+urlpatterns = [
+    path("promotion/create", create_promotion),
+    path("promotion/update", update_promotion),
+    path("promotion/<int:id>", delete_promotion),
+]
